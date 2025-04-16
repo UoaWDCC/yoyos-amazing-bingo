ad3b4850eb299a4e3c6fa885babf8d5e7d23e2ab
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2025-04-16 20:46:53 +1200

    :clown_face: make eslint highlighting work somehow and fix lint issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import useSWR from "swr";
 import { test } from "@/actions/test";
 
 export default function Home() {
-  const { data, error, isLoading } = useSWR("test", test);
+  const { data, isLoading } = useSWR("test", test);
 
   return isLoading ? <p>This is loading</p> : <h1>{data}</h1>;
 }
