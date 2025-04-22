211fa5c93f740bdf739e9b016d95b13bb051cbd3
Oculux314 <nate.williamson.nz@gmail.com>
2025-04-23 11:48:31 +1200

    :sparkles: implement useCompleteActivity

---
+++ src/actions/completeActivity.ts
@@ -0,0 +1,12 @@
+"use server";
+
+/**
+ * Complete the activity for a given team auth code and activity ID.
+ *
+ * @param code Team auth code.
+ * @param activityId Activity ID to complete.
+ */
+export async function completeActivity(code: string, activityId: string): Promise<void> {
+  // TODO: STUB
+  console.log(`completeActivity() called with code: ${code}, activityId: ${activityId}`);
+}
---
+++ src/queries/useCompleteActivity.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+import { completeActivity } from "@/actions/completeActivity";
+import { useSWRWithZod } from "@/utils/swr";
+
+/** @see completeActivity */
+export default function useCompleteActivity(code: string, activityId: string) {
+  return useSWRWithZod(
+    `activity/${code}/${activityId}`,
+    () => completeActivity(code, activityId),
+    z.void(),
+    "queries/useCompleteActivity.ts",
+  );
+}
