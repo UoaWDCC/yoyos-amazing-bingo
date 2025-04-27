22f4f0abb5b4fd8bab8c60f32792ea1bd72524ac
Oculux314 <nate.williamson.nz@gmail.com>
2025-04-27 13:20:50 +1200

    :sparkles: implement mutateTeam

---
+++ src/actions/updateTeam.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { Team } from "@/models/Team";
+
+/**
+ * Allow a team to update itself.
+ *
+ * @param team New team.
+ */
+export async function updateTeam(team: Team): Promise<void> {
+  // TODO: Implement the update logic here
+  console.log(`updateTeam() called with team: ${team.name}`);
+}
