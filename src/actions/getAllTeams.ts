c0b82b65152ab7dd9f85b345ffcff94eac5629c6
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-04-27 12:58:11 +1200

    added getteam with dummy data

--- src/actions/getAllTeams.ts
+++ src/actions/getAllTeams.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { Team } from "@/models/Team";
+import {getTeams} from "@/services/getTeams";
 
 /**
  * Fetches all teams (including boards).
@@ -9,29 +10,39 @@ import { Team } from "@/models/Team";
  * @returns All teams (including boards).
  */
 export async function getAllTeams(code: string): Promise<Team[]> {
-  // TODO: STUB
-  console.log(code);
-
+  const res = await getTeams() as Team[];
   const dummyBoard = [
     [false, false, false, false],
     [false, false, false, false],
     [false, false, false, false],
     [false, false, false, false],
   ];
-
-  const dummyTeam = {
-    id: "dummyId",
-    name: "Dummy Team",
-    code: "dummyCode",
-    board: dummyBoard.concat(),
-  };
-
-  const secondDummyTeam = {
-    id: "dummyId2",
-    name: "Second Dummy Team",
-    code: "dummyCode2",
-    board: dummyBoard.concat(),
-  };
-
-  return [dummyTeam, secondDummyTeam];
+  const newRes = res.map((row) => {
+    return {
+      id: row.id,
+      name: row.name,
+      code: row.code,
+      board: dummyBoard,
+    }
+  })
+  return newRes;
+  // // TODO: STUB
+  // console.log(code);
+  //
+  //
+  // const dummyTeam = {
+  //   id: "dummyId",
+  //   name: "Dummy Team",
+  //   code: "dummyCode",
+  //   board: dummyBoard.concat(),
+  // };
+  //
+  // const secondDummyTeam = {
+  //   id: "dummyId2",
+  //   name: "Second Dummy Team",
+  //   code: "dummyCode2",
+  //   board: dummyBoard.concat(),
+  // };
+  //
+  // return [dummyTeam, secondDummyTeam];
 }
