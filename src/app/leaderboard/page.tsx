6e90a9258d8b4b781c46a3c3899618236f42fa5b
Andrew Qiu <git@andrewqiu.com>
2025-04-27 12:43:57 +1200

    create base leaderboard page

---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,17 @@
+import { NormalLayout } from "@/components/ui/layout/NormalLayout";
+import { Pill } from "@/components/ui/pill";
+
+export default function Leaderboard() {
+
+  return (
+    <NormalLayout title="Leaderboard">
+      <div className="flex flex-col gap-8">
+        sdiahskfhdsuikhfikjbdgjfhkg
+      </div>
+
+      <div>
+        dskjhafjsdjklhgvjklhgfj
+      </div>
+    </NormalLayout>
+  );
+}
