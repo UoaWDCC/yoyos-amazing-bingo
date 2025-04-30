1110a7ac2dbb6d246a861bf70dbb8311d471d51f
Oculux314 <nate.williamson.nz@gmail.com>
2025-04-30 23:31:31 +1200

    :bricks: schema changes for frontend

--- src/models/Activity.ts
+++ src/models/Activity.ts
@@ -7,8 +7,9 @@ export const ActivitySchema = z.object({
   id: IdSchema,
   name: z.string().min(1).max(255),
   code: CodeSchema,
+  cardImageName: z.string().url(), // The URL of the card image for the activity
   description: z.string(),
-  basePoints: z.number().int().min(0), // The base points for the activity
+  basePoints: z.number().int().min(0), // The base points for the activity, before team modifiers
   boardOrder: z.number().int().min(0).max(15), // The order in which the activity appears on the board (0-indexed, in reading order)
 });
 
