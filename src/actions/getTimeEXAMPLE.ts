025ad3f6e911be33cd286dcb9f2a03be4c5d6463
Oculux314 <nate.williamson.nz@gmail.com>
2025-04-30 14:07:01 +1200

    :fire: delete examples

--- src/actions/getTimeEXAMPLE.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import "server-only";
-
-/** Just an example server action to return the current server time */
-export async function getTime() {
-  console.log(`getTime() called at: ${new Date().toLocaleTimeString()}`);
-  return `getTime() called at: ${new Date().toLocaleTimeString()}`;
-}
--- src/app/(auth)/page.client.tsx
+++ src/app/(auth)/page.client.tsx
@@ -2,24 +2,14 @@
 
 import useGetAllTeams from "@/queries/useGetAllTeams";
 import useGetTeam from "@/queries/useGetTeam";
-import useGetTime from "@/queries/useTimeEXAMPLE";
 
 export default function Home() {
   // High-quality integration testing
-  const { data: time, isLoading: timeIsLoading } = useGetTime();
   const { data: allTeams, isLoading: allTeamsIsLoading } = useGetAllTeams();
   const { data: team, isLoading: teamIsLoading } = useGetTeam("ausa");
 
   return (
     <>
-      <div>
-        {" "}
-        {timeIsLoading ? (
-          <p className="bg-red-200">Time is loading</p>
-        ) : (
-          <h1 className="bg-red-200">{time}</h1>
-        )}
-      </div>
       {teamIsLoading ? (
         <p className="bg-emerald-200">Loading team...</p>
       ) : (
