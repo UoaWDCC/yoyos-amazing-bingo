506feb47c5811c37389da8f66d404af0bbe61057
Tulza <Tulza0410@gmail.com>
2025-05-01 15:24:35 +1200

    feat: added pokemons

--- src/assets/pokecards/index.ts
+++ src/assets/pokecards/index.ts
@@ -1,10 +1,37 @@
 import alphaMask from "./alpha.webp";
 import backImage from "./backside.webp";
+import backside from "./backside.webp";
+import bulbasour from "./bulbasour.webp";
+import jigglybuff from "./jigglybuff.webp";
+import pikachoo from "./pikachoo.webp";
+import pokewebster from "./pokewebster.webp";
+import psyweb from "./psyweb.webp";
 import snorelaxImage from "./snorelax.webp";
-import testImage from "./test.webp";
+import warill from "./warill.webp";
+import warizard from "./warizard.webp";
+import webbykarp from "./webbykarp.webp";
+import wengar from "./wengar.webp";
+import wewtwo from "./wewtwo.webp";
+import witto from "./witto.webp";
+import worterra from "./worterra.webp";
 
 export const cards = {
   mask: alphaMask,
   back: backImage,
-  images: [snorelaxImage, testImage],
+  images: [
+    snorelaxImage,
+    backside,
+    bulbasour,
+    jigglybuff,
+    pikachoo,
+    pokewebster,
+    psyweb,
+    warill,
+    warizard,
+    webbykarp,
+    wengar,
+    wewtwo,
+    witto,
+    worterra,
+  ],
 };
