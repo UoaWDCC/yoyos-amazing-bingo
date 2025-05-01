dcbe4321cb1fdd9b677d450f3a91a89531e8668d
AntGa <antonvince.garay@gmail.com>
2025-05-02 00:53:15 +1200

    ✨ frontend ui

--- src/components/ui/layout/Header.tsx
+++ src/components/ui/layout/Header.tsx
@@ -1,7 +1,9 @@
+// components/Header.tsx
 import { auth } from "@/actions/authActions";
 import { getTeamAction } from "@/actions/getTeamAction";
 
 import { Heading } from "../heading";
+import TeamNameEditor from "../heading/TeamNameEditor";
 
 /**
  * This is the header component for the app.
@@ -9,10 +11,11 @@ import { Heading } from "../heading";
  * @param title - The title of the page.
  * @returns The header component.
  */
+
 export default async function Header({ title }: { title: string }) {
   // THIS IMPLEMENTATION MEANS THAT THE NormalLayout COMPONENT MUST BE SERVER SIDE RENDERED
   const { teamId } = await auth();
-  const teamName = teamId ? (await getTeamAction(teamId))?.name : "";
+  const team = teamId ? await getTeamAction(teamId) : null;
 
   return (
     <div className="flex justify-between">
@@ -24,7 +27,7 @@ export default async function Header({ title }: { title: string }) {
         {/* technically for the non existance SEO */}
         <Heading.h1>{title || "Untitled"}</Heading.h1>
       </div>
-      <p>{teamName}</p>
+      {team && <TeamNameEditor teamId={team.id} initialName={team.name} />}
     </div>
   );
 }
