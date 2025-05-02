07ff0ea1df541d61b0bb4ac39c348f6775579c16
Oculux314 <nate.williamson.nz@gmail.com>
2025-05-02 21:22:55 +1200

    :sparkles: add redirect from admin if not admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,16 @@
 import { NormalLayout } from "@/components/ui/layout/NormalLayout";
 
 import { GameStatusField } from "./_components/GameStatusField";
+import { auth } from "@/actions/authActions";
+import env from "@/lib/env";
+import { redirect } from "next/navigation";
 
-export default function AdminPage() {
+export default async function AdminPage() {
+  const  {teamId} = await auth();
+  if (teamId !== env.ADMIN_ID) {
+    redirect("/board");
+  }
+
   return (
     <NormalLayout title="Admin">
       <GameStatusField />
