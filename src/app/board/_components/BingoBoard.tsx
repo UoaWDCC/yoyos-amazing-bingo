8d48ca7342817a6ebb94723f95bc549a041c996e
Oculux314 <nate.williamson.nz@gmail.com>
2025-05-03 00:04:53 +1200

    :sparkles: hook up board page

--- src/app/board/_components/BingoBoard.tsx
+++ src/app/board/_components/BingoBoard.tsx
@@ -1,45 +1,25 @@
-"use client";
-
-import { useMemo } from "react";
-
 import { ActivityDrawer } from "./ActivityDrawer";
+import useAuth from "@/queries/useAuth";
+import { redirect } from "next/navigation";
+import useGetTeam from "@/queries/useGetTeam";
 
-type BingoBoardProps = {
-  teamId: string;
-};
+export function BingoBoard() {
+  const { data: teamId } = useAuth();
+  if (teamId === "admin") {
+    redirect("/admin");
+  }
+  const { data: team } = useGetTeam(teamId ?? null);
 
-export function BingoBoard({ teamId }: BingoBoardProps) {
-  console.log(teamId);
+  if (!team) {
+    return "Loading..."
+    // return <BingoBoardSkeleton />;
+  }
 
-  // mock data
-  const squares = Array.from({ length: 16 }).map((_, index) => ({
-    isCompleted: Math.random() > 0.8,
-    points: 1 + Math.floor(Math.random() * 3),
-    activity: {
-      id: `${index}`,
-      name: `Activity ${index}`,
-      description: `Description ${index}`,
-      code: `code-${index}`,
-      cardImageName: `image-${index}.png`,
-      basePoints: 1 + Math.floor(Math.random() * 3),
-      boardOrder: index,
-    },
-  }));
-
-  const sortedSquares = useMemo(() => {
-    return squares?.sort((a, b) => {
-      return a.activity.boardOrder - b.activity.boardOrder;
-    });
-  }, [squares]);
-
-  // TODO: add loading skeleton back
-  // if (isLoading || !sortedSquares) {
-  //   return <BingoBoardSkeleton />;
-  // }
+  const squares = team.board;
 
   return (
     <div className="grid grid-cols-4 gap-2 px-8">
-      {sortedSquares.map((square, index) => (
+      {squares.map((square, index) => (
         <ActivityDrawer
           key={square.activity.boardOrder}
           teamActivity={square}
