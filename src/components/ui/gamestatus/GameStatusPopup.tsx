49580e34d30597e6872e48c8502417f1eec013b6
Oculux314 <nate.williamson.nz@gmail.com>
2025-05-02 20:37:19 +1200

    :sparkles: allow yoyover to be dismissed

--- src/components/ui/gamestatus/GameStatusPopup.tsx
+++ src/components/ui/gamestatus/GameStatusPopup.tsx
@@ -2,17 +2,39 @@
 
 import useGetGameStatus from "@/queries/useGetGameStatus";
 
+import { ReactNode, useState } from "react";
+
 import Yoyover from "./Yoyover";
 
 export function GameStatusPopup() {
   const { data } = useGetGameStatus();
-  console.log("GameStatusPopup", data);
 
   if (data === "yoyover") {
-    return <Yoyover />;
+    return (
+      <Backdrop>
+        <Yoyover />
+      </Backdrop>
+    );
   } else if (data === "finished") {
-    return <></>; // TODO: that page we talked about Ashton
+    return (
+      <Backdrop>
+        <p>...</p>
+      </Backdrop>
+    ); // TODO: that page we talked about Ashton
   } else {
     return null;
   }
+}
+
+function Backdrop({ children }: { children: ReactNode }) {
+  const [isPopupDismissed, setIsPopupDismissed] = useState(false);
+
+  return (
+    <div
+      onClick={() => {console.log("wow"); setIsPopupDismissed(true);}}
+      className={`${isPopupDismissed ? "hidden" : "fixed"} inset-0 flex h-dvh w-dvw flex-col items-center justify-center gap-4 bg-black/40 px-4`}
+    >
+      {children}
+    </div>
+  );
 }
--- src/components/ui/gamestatus/Yoyover.tsx
+++ src/components/ui/gamestatus/Yoyover.tsx
@@ -6,13 +6,13 @@ import { Pill } from "../pill";
 
 const Yoyover = () => {
   return (
-    <div className="fixed inset-0 flex h-dvh w-dvw flex-col items-center justify-center gap-4 bg-black/40 px-4">
+    <>
       <div className="aspect relative flex aspect-3/4 w-full max-w-[600px] items-center overflow-hidden rounded-xl bg-white">
         <Image src={yoyovergif} className="object-cover" fill alt="Yoyover" />
       </div>
       <Pill>It&apos;s yoyover</Pill>
       <Pill>Please return to 405-422 :D</Pill>
-    </div>
+    </>
   );
 };
 
