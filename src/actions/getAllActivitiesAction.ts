ea00d98c807cd1e26337fdf1a9ee8a7d9e8c415c
AntGa <antonvince.garay@gmail.com>
2025-05-21 11:32:59 +1200

    🚨 linting 😾

--- src/actions/getAllActivitiesAction.ts
+++ src/actions/getAllActivitiesAction.ts
@@ -2,8 +2,6 @@
 
 import "server-only";
 
-import { db } from "@/db/connection";
-import { activitiesTable } from "@/db/schema";
 import { getAllActivitiesService } from "@/services/getAllActivitiesService";
 
 export async function getAllActivitiesAction() {
