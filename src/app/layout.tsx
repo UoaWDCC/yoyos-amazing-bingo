3c8f1a6dfeb33c29569b29fa68772124eceff1af
Brandon Seng Han Chan <bshchan.theone@gmail.com>
2026-05-14 12:55:24 +1200

    chore: update the layout.tsx metadata and openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,27 +7,28 @@ import "@/assets/globals.css";
 import RevalidationComponent from "@/revalidation/RevalidationComponent";
 
 export const metadata: Metadata = {
-  title: "Webster's Alternate Reality | WDCC",
+  title: "Webster's Villain Arc | WDCC",
   icons: {
     icon: "./favicon.ico",
   },
-  description: "Introducing: Webster's Alternate Reality by WDCC!!",
+  description: "Introducing: Webster's Villain Arc by WDCC!!",
   keywords: [
     "Web Development & Consulting Club",
     "WDCC",
     "Yoyo's Amazing Bingo", // Hey, just in case people are still searching for this
-    "Webster's Alternate Reality",
-    "ARG",
+    "Webster's Alternate Reality", // Hey, just in case people are still searching for this
+    "Webster's Villain Arc",
+    "Bingo",
     "Game",
   ],
 
   // description display for when you post the link on social media
   openGraph: {
-    title: "Webster's Alternate Reality | Web Development & Consulting Club",
-    description: "Introducing: Webster's Alternate Reality by WDCC!!",
+    title: "Webster's Villain Arc | Web Development & Consulting Club",
+    description: "Introducing: Webster's Villain Arc by WDCC!!",
     url: "https://bingo.wdcc.co.nz/",
     images: "https://wdcc.co.nz/images/og-image.png",
-    siteName: "Webster's Alternate Reality | Web Development & Consulting Club",
+    siteName: "Webster's Villain Arc | Web Development & Consulting Club",
   },
 };
 
